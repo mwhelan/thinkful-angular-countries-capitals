1e04929125ed4e093155c3dab0a8938cd943896a
Michael Whelan <mjmwdev@gmail.com>
2014-07-06 13:18:17 +0100

    country list with services, country detail with some services

---
+++ countries/countries.js
@@ -0,0 +1,18 @@
+﻿viewsModule.config([
+    "$routeProvider", function($routeProvider) {
+        $routeProvider.when("/countries", {
+            templateUrl: "./countries/countries.html",
+            controller: "CountriesController"
+        });
+    }
+]);
+
+viewsModule.controller('CountriesController', [
+    '$scope', 'cacCountryList',
+    function($scope, cacCountryList) {
+        cacCountryList.getCountries().then(function(result) {
+            $scope.data = result.data;
+        });
+    }
+]);
+                              
